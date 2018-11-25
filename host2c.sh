040d1e254552b03eafbebd51afb3d8d7ad3bc01e
juridalvai <44428310+juridalvai@users.noreply.github.com>
2018-11-25 11:25:18 +0100

    upload files host2c.sh ed router2.sh
    uploaded files for the provisioning of host2c ed router2

---
+++ host2c.sh
@@ -0,0 +1,24 @@
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce --assume-yes --force-yes
+ip link set dev eth1 up
+ip add add 192.168.40.1/30 dev eth1
+ip route add 192.168.0.0/16 via 192.168.40.2
+docker rm $(docker ps -a -q)
+docker ps
+docker run -dit --name tecmint-web -p 32768:80 -v /home/user/website/:/usr/local/apache2/htdocs/ httpd:2.4
+docker ps
+echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment of Design of Network and Communication System</title>
+</head>
+<body>
+    <h1>If you reach thi static web page and you visualize it correctly means that the configuration is correct!</h1>
+    <h2>This project was made by Juri Dalvai and Tobia Ducoli.</h2>
+</body>
+</html>"> /home/user/website/docker.html
---
+++ router2.sh
@@ -0,0 +1,18 @@
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y tcpdump apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-key add -
+add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
+apt-get update
+apt-get install -y frr --assume-yes --force-yes
+ip link set dev eth1 up
+ip add add 192.168.40.2/30 dev eth1
+ip link set dev eth2 up
+ip add add 192.168.30.2/30 dev eth2
+sysctl net.ipv4.ip_forward=1
+sed -i "s/\(zebra *= *\).*/\1yes/" /etc/frr/daemons
+sed -i "s/\(ospfd *= *\).*/\1yes/" /etc/frr/daemons
+service frr restart
+service frr status
+vtysh -e 'conf t' -e 'router ospf' -e 'redistribute connected' -e 'exit' -e 'interface eth2' -e 'ip ospf area 0.0.0.0' -e 'exit' -e 'exit' -e 'write'
+echo " ">>/etc/frr//frr.conf
