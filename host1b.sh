d19372040b8635c8ae44825053581f63984c4659
tobiaducoli <44766862+tobiaducoli@users.noreply.github.com>
2018-11-25 11:14:39 +0100

    Update and rename docker.sh to host1b

--- host1b.sh
+++ host1b.sh
@@ -4,3 +4,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce --assume-yes --force-yes
+ip link set dev eth1 up
+ip add add 192.168.20.1/27 dev eth1
+ip route add 192.168.0.0/16 via 192.168.20.30
