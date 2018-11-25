f497606ea5bf2b8fd0ee61a55874c16412e2beda
tobiaducoli <44766862+tobiaducoli@users.noreply.github.com>
2018-11-25 16:29:10 +0100

    Update host1b.sh

--- host1b.sh
+++ host1b.sh
@@ -1,9 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce --assume-yes --force-yes
+apt-get install -y curl --assume-yes --force-yes
+apt-get install -y tcpdump --assume-yes
 ip link set dev eth1 up
 ip add add 192.168.20.1/27 dev eth1
 ip route add 192.168.0.0/16 via 192.168.20.30
