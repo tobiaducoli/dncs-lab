aef5df00a714de17c141146b0915c90f08738492
tobiaducoli <44766862+tobiaducoli@users.noreply.github.com>
2018-11-25 16:30:11 +0100

    Update host1b.sh

--- host1b.sh
+++ host1b.sh
@@ -1,6 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y curl --assume-yes --force-yes
+apt-get install -y curl --assume-yes
 apt-get install -y tcpdump --assume-yes
 ip link set dev eth1 up
 ip add add 192.168.20.1/27 dev eth1
